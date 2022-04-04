0efc453430f7cc2b6060e1fedd7791a242d43d31
youbaidjis <youba.etud@gmail.com>
2022-04-03 22:34:30 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    
+    for (const user of record) {
+        if (user.result === "W"){
+            return user.year
+        }
+      }
+    
+}
+
